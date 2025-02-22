c5f575e4781b6b5cbc6123ce5c3c74ef17a198b0
Sam Spain <sam@samspain.com>
2025-02-22 10:12:52 +0000

    Movement between 3 points

---
+++ Player.cs
@@ -0,0 +1,90 @@
+using Godot;
+using static Godot.GD;
+
+public partial class Player : CharacterBody3D
+{
+	// How fast the player moves in meters per second.
+	[Export]
+	public int Speed { get; set; } = 100;
+	// The downward acceleration when in the air, in meters per second squared.
+	[Export]
+	public int FallAcceleration { get; set; } = 150;
+	
+	[Export]
+	public int JumpImpulse { get; set; } = 80;
+
+	[Export]
+	public Node3D targetNode1;
+
+	[Export]
+	public Node3D targetNode2;
+
+	[Export]
+	public Node3D targetNode3;
+
+
+	private int currentTargetNode = 1;
+
+	private Vector3 _targetVelocity = Vector3.Zero;
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var direction = Vector3.Zero;
+
+		if (Input.IsActionJustPressed("move_up"))
+		{
+			Print("Pressed Up");
+			if(currentTargetNode == 0 || currentTargetNode == 1) {
+				currentTargetNode++;
+			}
+			Print("Current Target Node: " + currentTargetNode);
+		}
+		if (Input.IsActionJustPressed("move_down"))
+		{
+			Print("Pressed Down");
+			if(currentTargetNode == 1 || currentTargetNode == 2) {
+				currentTargetNode--;
+			}
+			Print("Current Target Node: " + currentTargetNode);
+		}
+
+
+		if(currentTargetNode == 0) {
+			// Move towards targetNode1
+			direction = GlobalPosition.DirectionTo(targetNode1.Position);
+		} else if(currentTargetNode == 1) {
+			direction = GlobalPosition.DirectionTo(targetNode2.Position);
+		}
+		else if(currentTargetNode == 2) {
+			direction = GlobalPosition.DirectionTo(targetNode3.Position);
+		}
+
+
+
+		if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+		{
+			_targetVelocity.Y = JumpImpulse;
+		}
+
+		if (direction != Vector3.Zero)
+		{
+			direction = direction.Normalized();
+			//GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+		}
+		
+		// Ground velocity
+		_targetVelocity.X = direction.X * Speed;
+		_targetVelocity.Z = direction.Z * Speed;
+
+		// Vertical velocity
+		if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
+		{
+			_targetVelocity.Y -= FallAcceleration * (float)delta;
+		}
+
+		// Moving the character
+		Velocity = _targetVelocity;
+		MoveAndSlide();
+	}
+}
