de7531d71bb4bb5d58f47808f38df7f42173c2db
Sam Spain <sam@samspain.com>
2025-02-22 10:37:17 +0000

    Chickens moving on the map

---
+++ assets/Meshes/Chicken.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Chicken : CharacterBody3D
+{
+
+	[Export]
+	public int Speed { get; set; } = 100;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	private Vector3 _targetVelocity = Vector3.Zero;
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_targetVelocity.X = -1 * Speed;
+		Velocity = _targetVelocity;
+		MoveAndSlide();
+	}
+}
